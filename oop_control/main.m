801598cf0caa043549d568620151ef0b5e751e1c
Theo <theo.guegan@etu.utc.fr>
2024-04-03 22:44:10 +0200

    ras

--- oop_control/main.m
+++ oop_control/main.m
@@ -4,10 +4,10 @@ pos_obst = load("obstacles.txt");
 targets = load("target.txt");
 
 %init obstacles
-obst = [];
+obstacles = [];
 for i = 1:size(pos_obst, 1)
-    obst = [obst, obstacle(pos_obst(i, 1), pos_obst(i, 2), pos_obst(i, 3), pos_obst(i, 4))];
-    obst(i).plot();
+    obstacles = [obstacles, obstacle(pos_obst(i, 1), pos_obst(i, 2), pos_obst(i, 3), pos_obst(i, 4))];
+    obstacles(i).plot();
 end
 
 %init vehicle
@@ -29,10 +29,10 @@ for j = 1:size(targets, 1)
     % Move towards the target until reaching it
     i=0
     while(robot.get_distance_point(target) > 0.5)
-        res = robot.controller_selection(obst, target);
+        res = robot.controller_selection(obstacles, target);
         robot.update_pos(res);
         i=i+1
-        if (~rem(i, 5))
+        if (~rem(i, 10)) %tous les multiple de 10
             robot.plot();
         end
     end
