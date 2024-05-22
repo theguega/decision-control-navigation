d4bb2defe98ec749c4224d41844689fce0688249
Alexandre Borghi <alexandre.borghi@etu.utc.fr>
2024-05-22 09:06:21 +0200

    fix sign error in angular error making cars turn again

--- src/tools/calc/subtr_ang.m
+++ src/tools/calc/subtr_ang.m
@@ -1,12 +1,11 @@
 function E_Theta=subtr_ang(Theta_T, ThetaReel)
     E_Theta = Theta_T - ThetaReel;
     if E_Theta > pi
-        E_Theta = -2*pi + E_Theta;
+        E_Theta =  E_Theta - 2*pi;
+    elseif E_Theta < -pi
+        E_Theta = E_Theta + 2*pi;
     end
-    if E_Theta < -pi
-        E_Theta = -2*pi - E_Theta;
-    end
-    if abs(E_Theta)> (pi/2)
+    if abs(E_Theta) > (pi/2)
         E_Theta = sign(E_Theta)*((pi/2) - 0.06);
     end
 end
